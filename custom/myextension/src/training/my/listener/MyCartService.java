d394aba3fb817398788dd1c91ffac637abed8c58
serhii-slobodianiuk <sergey.slobodianiuk@gmail.com>
2021-07-14 22:57:19 +0300

    Tasks of the second modulehave been done

---
+++ custom/myextension/src/training/my/listener/MyCartService.java
@@ -0,0 +1,25 @@
+package training.my.listener;
+
+import de.hybris.platform.core.model.order.CartModel;
+import de.hybris.platform.core.model.product.ProductModel;
+import de.hybris.platform.core.model.product.UnitModel;
+import de.hybris.platform.order.impl.DefaultCartService;
+import de.hybris.platform.servicelayer.event.EventService;
+import de.hybris.platform.servicelayer.model.ModelService;
+
+import javax.annotation.Resource;
+
+public class MyCartService extends DefaultCartService {
+    @Resource
+    EventService eventService;
+
+    @Resource
+    ModelService modelService;
+
+    @Override
+    public void addToCart(final CartModel cartModel, final ProductModel productModel, final long quantity,
+                          final UnitModel unitModel) {
+        modelService.save(cartModel);
+        eventService.publishEvent(new MyEvent());
+    }
+}
